e26674b2df3465e22bde5e83f038b838eca9e9bb
farhanafiffendi <farhanafiffendi19@gmail.com>
2022-06-23 11:33:30 +0700

    first commit

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const express = require('express')
 const router = require('./src/routes')
 require('dotenv').config()
 
+const cors = require("cors");
+
 const app = express()
 
-const port = 5000
+const port = process.env.PORT || 5000;
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
+
 
 // Add endpoint grouping and router
 app.use('/api/v1/', router)
