e2ea6c02750b37492ac7480616b12424aa3c21d5
farhanafiffendi <farhanafiffendi19@gmail.com>
2022-06-23 12:20:07 +0700

    Update

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,9 +6,19 @@ const { register, login, checkLogin } = require('../controllers/auth')
 
 const { auth } = require('../middlewares/auth')
 
+const { getUsers, getUser, getUserTrans } = require('../controllers/user')
+
 // route
 router.post('/register', register)
 router.post('/login', login)
 router.get('/check', auth, checkLogin)
+
+router.get('/users', getUsers)
+//
+
+router.get("/user/:id", getUser);
+
+router.get("/userTrans", auth, getUserTrans);
+
 
 module.exports = router
